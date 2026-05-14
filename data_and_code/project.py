b4100ebe7b73c78ff9864c68b8572ce713e9acc8
Lily Ainslie <lily_ainslie@yahoo.com>
2026-05-14 12:23:19 +0200

    Updated file organisation

--- data_and_code/project.py
+++ data_and_code/project.py
@@ -1,8 +1,12 @@
 import glob
 import xml.etree.ElementTree as et
+import os
 
-#look at python website for documentation
+os.chdir(r"c:/Users/lily_/OneDrive/Documents/Masters/S2/Computational_Social_Science/comp_soc_sci/data_and_code")
+print(os.getcwd())
 
 #use glob to get xml files. store as variable paths /*.xml
+paths = glob.glob("scrutins.xml.zip/*")
 
+print(paths)
 #do a for loop for path in paths keep only laws where eg 20% of deputes voted (then see how many laws left. maybe adjust the percentage)
