981903312c91dba76585de71e667d420dca9220a
Lily Ainslie <lily_ainslie@yahoo.com>
2026-05-14 10:41:54 +0200

    Adding datasets and nascent python file

---
+++ project.py
@@ -0,0 +1,8 @@
+import glob
+import xml.etree.ElementTree as et
+
+#look at python website for documentation
+
+#use glob to get xml files. store as variable paths /*.xml
+
+#do a for loop for path in paths keep only laws where eg 20% of deputes voted (then see how many laws left. maybe adjust the percentage)
